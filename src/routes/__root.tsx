780dfe603f49e9f77f8c811a763eebebffad4ebf
jongse7 <mmssjj72@naver.com>
2025-09-06 03:27:50 +0900

    feat: 로그인 페이지 UI

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { OverlayProvider } from 'overlay-kit';
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -8,7 +9,11 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <Outlet />
+      <div className="max-w-[768px] mx-auto flex justify-items-center scrollbar-hide">
+        <OverlayProvider>
+          <Outlet />
+        </OverlayProvider>
+      </div>
       {import.meta.env.DEV && <TanStackRouterDevtools />}
     </>
   );
